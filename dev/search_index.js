f127b776b8e8c3c4d61d39b463ee464b128f639c
Documenter.jl <documenter@juliadocs.github.io>
2022-09-16 16:45:14 +0000

    build based on cb8ae5c

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#LinearRationalExpectations.jl","page":"Home","title":"LinearRationalExpectations.jl","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for LinearRationalExpectations.jl","category":"page"}]
+}
